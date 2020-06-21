af4c4362d0deeb7d455e9e2ec1904eb19aa38ab4
Edgar Cardona <fabrikdev@gmail.com>
2020-06-21 11:48:01 -0500

    SETUP

---
+++ pages/about.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <div>
+      <h1>
+        About
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  components: {}
+})
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <div>
+      <h1>
+        Cardona IT
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  components: {}
+})
+</script>
+
+<style>
+
+</style>
