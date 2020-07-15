e988773476fbd1c87c5f81f26bbb2e8ca7c415ca
Edgar Cardona <fabrikdev@gmail.com>
2020-07-14 22:38:46 -0500

    FEAT: Users with image

--- pages/index.vue
+++ pages/index.vue
@@ -14,6 +14,9 @@
         <template v-slot:cell(enabled)="data">
           <b class="text-info">{{ data.value === 1 ? 'Yes' : 'No' }}</b>
         </template>
+        <template v-slot:cell(profile_photo_url)="data">
+          <b-img :src="data.value" alt="Profile" height="40px"></b-img>
+        </template>
       </b-table>
     </div>
   </div>
@@ -27,7 +30,10 @@ export default Vue.extend({
   components: {},
   data () {
     return {
-      fields: ['id', 'username', 'email', 'user_group', 'last_visit', 'enabled']
+      fields: ['id', {
+        key: 'profile_photo_url',
+        label: 'Profile'
+      }, 'username', 'email', 'user_group', 'last_visit', 'enabled']
     }
   },
   computed: {
