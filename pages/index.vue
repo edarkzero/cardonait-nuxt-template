d450ef108b4eca1a3db7095cdc119d185af6844d
Edgar Cardona <edgar.cardona@wundermanthompson.com>
2020-11-18 21:51:37 -0500

    Testing actions of tables

--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,19 @@
   <div class="container">
     <div>
       <h1>
-        Users - Count:{{users.total}}
+        Users - Count:{{ users.total }}
       </h1>
-      <b-table striped hover :items="users.rows" :fields="fields" :busy="users.length === 0" stacked="sm">
+      <b-table
+        striped
+        hover
+        :items="users.rows"
+        :fields="fields"
+        :busy="users.length === 0"
+        stacked="sm"
+      >
         <template v-slot:table-busy>
           <div class="text-center text-black-50 my-2">
-            <b-spinner class="align-middle"></b-spinner>
+            <b-spinner class="align-middle" />
             <strong>Loading...</strong>
           </div>
         </template>
@@ -15,7 +22,15 @@
           <b class="text-info">{{ data.value ? 'Yes' : 'No' }}</b>
         </template>
         <template v-slot:cell(profile_photo_url)="data">
-          <b-img :src="data.value" alt="Profile" height="40px"></b-img>
+          <b-img :src="data.value" alt="Profile" height="40px" />
+        </template>
+        <template #cell(actions)="row">
+          <b-btn size="sm" to="/user/detail">
+            <BIconPencil>{{ row.item.id }}</BIconPencil>
+          </b-btn>
+          <b-btn size="sm" to="/user/delete">
+            <BIconTrash>{{ row.item.id }}</BIconTrash>
+          </b-btn>
         </template>
       </b-table>
     </div>
@@ -25,15 +40,22 @@
 <script lang="ts">
 import Vue from 'vue'
 import { mapActions, mapGetters } from 'vuex'
+import { BIconPencil, BIconTrash } from 'bootstrap-vue'
 
 export default Vue.extend({
-  components: {},
+  components: {
+    BIconPencil,
+    BIconTrash
+  },
   data () {
     return {
-      fields: ['id', {
-        key: 'profile_photo_url',
-        label: 'Profile'
-      }, 'username', 'email', 'user_group', 'last_visit', 'enabled']
+      fields: [
+        'id', {
+          key: 'profile_photo_url',
+          label: 'Profile'
+        },
+        'username', 'email', 'user_group', 'last_visit', 'enabled',
+        { key: 'actions', label: 'Actions' }]
     }
   },
   computed: {
@@ -51,7 +73,3 @@ export default Vue.extend({
   }
 })
 </script>
-
-<style>
-
-</style>
